6ed75e65134281bc46a7422abb9583cb5370ca7d
rorschach <rorschach@darknet.darknet.com>
2026-08-17 23:16:28 -0300

    feat: remove Projects page and its navigation link

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,4 @@
 
 export default new Map([
-["src/content/posts/hello-world/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fhello-world%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/posts/teste/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fteste%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/hello-world/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fhello-world%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
